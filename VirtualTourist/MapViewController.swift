53dff5065e99030b55dbdf514e769ede3b0f2c2d
Venkat Kurapati <venkat@giffgaffmvenkat.local>
2017-01-04 15:49:20 +0000

    Implemented Mapview controller

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 //MARK:- showAlbum
 class MapViewController: UIViewController,MKMapViewDelegate,UIGestureRecognizerDelegate {
 
@@ -28,13 +29,14 @@ class MapViewController: UIViewController,MKMapViewDelegate,UIGestureRecognizerD
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //MARK:- Add Getusre recornizer to Mapview to dectect hold and drop
     func addGestureRecognizer(){
         let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(self.addPinAnnotationToMap(gestureRecognizer:)))
         lpgr.minimumPressDuration = 0.5   // half-second hold for  pin creation
         lpgr.delegate = self
         self.mapview.addGestureRecognizer(lpgr)
     }
+    //MARK:- Add pin to the Map as annotation
     func addPinAnnotationToMap(gestureRecognizer: UILongPressGestureRecognizer){
         if gestureRecognizer.state == UIGestureRecognizerState.began
         {
@@ -57,14 +59,97 @@ class MapViewController: UIViewController,MKMapViewDelegate,UIGestureRecognizerD
             stack.saveContext()
         }
     }
-    /*
+    
+    //MARK:- Mapview delegate methods
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapview.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil
+        {
+            // Force Initialize pinView.
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        }
+        else
+        {
+            pinView!.annotation = annotation
+        }
+        pinView!.pinTintColor = UIColor.red
+        pinView?.animatesDrop = true
+        pinView?.isDraggable = true
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView)
+    {
+        let annotation = view.annotation
+        mapview.selectAnnotation(annotation!, animated: false)
+        performSegue(withIdentifier: "showAlbum", sender: annotation )
+        
+        //Deselect the annotation here so that it's again selectable when we return from the album view:
+        mapView.deselectAnnotation(annotation, animated: true)
+    }
+    //MARK:- FetchPinsFromData
+    func loadPinsFromDatabase()
+    {
+        var pins = [Pin]()
+        var annotations = [MKAnnotation]()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
+        do
+        {
+            let results = try stack.persistentContainer.viewContext.fetch(fetchRequest)
+            if let results = results as? [Pin]
+            {
+                pins = results
+                print("Number of Pins : \(pins.count)")
+            }
+        }
+        catch
+        {
+            print("Couldn't find any Pins")
+        }
+        
+        for (_,item) in pins.enumerated()
+        {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(item.latitude),longitude: CLLocationDegrees(item.longitude))
+            annotations.append(annotation)
+        }
+        mapview.addAnnotations(annotations)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showAlbum"
+        {
+            // First find the selected Pin in Core Data, if found send the associated Pin annotation to PhotoViewController.
+            var pin: Pin!
+            do
+            {
+                let pinAnnotation = sender as! MKAnnotation
+                
+                let fetchRequest = NSFetchRequest<Pin>(entityName: "Pin")
+                let predicate = NSPredicate(format: "latitude == %@ AND longitude == %@", argumentArray: [pinAnnotation.coordinate.latitude, pinAnnotation.coordinate.longitude])
+                fetchRequest.predicate = predicate
+                let pins = try stack.persistentContainer.viewContext.fetch(fetchRequest)
+                
+                pin = pins[0]
+            }
+            catch let error as NSError
+            {
+                print("failed to get pin by object id")
+                print(error.localizedDescription)
+                return
+            }
+            
+            //let photosVC = segue.destination as! PhotoAlbumViewController
+           // photosVC.pin = Converter.toPin(sender as! MKAnnotation, pin)
+        }
     }
-    */
+    
 
 }
